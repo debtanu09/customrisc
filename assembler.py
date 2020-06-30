5392dc96d282be62bde7871d46bd83a6c3b43c6c
Debtanu Mukherjee <debtanu09@gmail.com>
2020-06-30 13:06:15 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 cmds = []
 string = []
-co = {'add':'0000', 'adc':'0000', 'adz':'0000', 'adi':'0001', 'ndu':'0010', 'ndc':'0010', 'ndz':'0010', 'lw':'0100', 'sw':'0101', 'lm':'0110', 'sm':'0111', 'lhi':'0011', 'beq':'1100', 'jal':'1000', 'jlr':'1001', 'mvi':'1010', 'mov':'1011'}
+co = {'add':'0000', 'adc':'0000', 'adz':'0000', 'adi':'0001', 'ndu':'0010', 'ndc':'0010', 'ndz':'0010', 'lw':'0100', 'sw':'0101', 'lhi':'0011', 'beq':'1100', 'jal':'1000', 'jlr':'1001', 'mvi':'1010', 'mov':'1011'}
 ex = {'add':'000', 'adc':'010', 'adz':'001', 'ndu':'000', 'ndc':'010', 'ndz':'001'}
 r = {'r0':'000','r1':'001','r2':'010','r3':'011','r4':'100','r5':'101','r6':'110','r7':'111'}
 with open('code.asm','r') as f:
@@ -12,7 +12,7 @@ with open('code.asm','r') as f:
 			string.append(co[a[0]]+r[a[2]]+r[a[3]]+r[a[1]]+ex[a[0]])
 		elif a[0] == 'adi':
 			string.append(co[a[0]]+r[a[2]]+r[a[1]]+a[3])
-		elif a[0] == 'sm' or a[0] == 'lm' or a[0] == 'lhi' or a[0] == 'jal' or a[0] == 'mvi':
+		elif a[0] == 'lhi' or a[0] == 'jal' or a[0] == 'mvi':
 			string.append(co[a[0]]+r[a[1]]+a[2])
 		elif a[0] == 'lw' or a[0] == 'sw' or a[0] == 'beq':
 			string.append(co[a[0]]+r[a[1]]+r[a[2]]+a[3])
