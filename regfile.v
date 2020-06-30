b9c15c39ffd6ef27f1474378c1db63a52f3e51bd
Debtanu Mukherjee <debtanu09@gmail.com>
2020-06-30 13:07:29 +0530

    Update regfile.v

--- regfile.v
+++ regfile.v
@@ -27,11 +27,4 @@ module regfile(clk, rst, regen, outaddr1, outaddr2, out1, out2, inaddr, in, pcou
 	assign out1 = memory[outaddr1];
 	assign out2 = memory[outaddr2];
 	assign pcout = memory[7];
-	
-	initial begin
-	//$display("REG 0 REG 1 REG 2 REG 3 REG 4 REG 5 REG 6 REG 7");
-	//$monitor("%4h %4h %4h %4h %4h %4h %4h %4h %0t",memory[0],memory[1],memory[2],memory[3],memory[4],memory[5],memory[6],memory[7], $time);
-	//$monitor("%0d %0d %d",memory[0], memory[1], memory[2]);
-	$monitor("%0d",memory[2]);
-	end
 endmodule
